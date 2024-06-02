7f3789df57a50c49d68de0ea599eb226dcdc138a
Lauren <lvavila80@ucatolica.edu.co>
2024-06-02 15:21:17 -0500

    COnfiguración sobre el uso de las importaciones y exportaciones de los paquetes y clases.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+// src/App.js
+import React from 'react';
+import CrudComponent from './CrudComponent';  // Asegúrate de que el nombre del archivo y la ruta sean correctos
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <CrudComponent />
     </div>
   );
 }
